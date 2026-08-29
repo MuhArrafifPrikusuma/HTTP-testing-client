46556e3dbd92c7f6fa6aecae243004880ba8fe31
MuhArrafifPrikusuma <muhammadarrafif1308@gmail.com>
2026-08-30 05:40:08 +0800

    async concurrent (unfinished)

--- src/Request.zig
+++ src/Request.zig
@@ -1,5 +1,14 @@
 const std = @import("std");
 
+pub const Client = struct {
+    // fuzz = true for auto generate data
+    fuzz: bool = false,
+    // how many times to repeat this, if you combine this with fuzz
+    // it will generate different data on every request
+    repeat: u32 = 0,
+    request: Request = undefined,
+};
+
 const Methods = enum {
     GET,
     POST,
@@ -10,58 +19,51 @@ const Methods = enum {
 const Request = struct {
     method: Methods,
     path: []const u8,
-    host: []const u8 = "localhost:8080", // <- automatically filled
-    agent: []const u8 = "idk-for-now/tester",
+    host: []const u8 = "localhost:8080",
+    agent: []const u8 = "idk-for-now/tester", // <- automatically filled
     body: []const u8,
 };
 
-pub const Client = struct {
-    // fuzz = true for auto generate data
-    fuzz: bool = false,
-    // how many times to repeat this, if you combine this with fuzz
-    // it will generate different data on every request
-    repeat: u32 = 0,
-    // ^ that one is just for parsing use atomic_repeat with repeat as it's value for
-    // thread safe counter
-    atomic_repeat: std.atomic.Value(u32) = .init(0),
-    request: Request = undefined,
-};
-
 /// initiate client arena allocator
-pub const ClintInterface = struct {
+pub const ClientInterface = struct {
     arena: std.heap.ArenaAllocator,
-    client: Client,
+    uri: []const u8 = undefined,
+    client: []Client,
 
-    pub fn init(backing_allocator: std.mem.Allocator) !*ClintInterface {
+    pub fn init(backing_allocator: std.mem.Allocator) !*ClientInterface {
         var arena = std.heap.ArenaAllocator.init(backing_allocator);
         errdefer arena.deinit();
 
         const allocator = arena.allocator();
 
-        const self = try allocator.create(ClintInterface);
+        const self = try allocator.create(ClientInterface);
 
         self.* = .{
             .arena = arena,
-            .client = .{},
+            .client = undefined,
         };
         return self;
     }
 
-    pub fn deinit(self: *ClintInterface) !void {
+    pub fn deinit(self: *ClientInterface) !void {
         self.arena.deinit();
     }
 };
 
-pub fn requestHandler(io: std.Io) !void {
-    const allocator = std.heap.smp_allocator;
+pub const Shared = struct {
+    repeat: []std.atomic.Value(u32) = .init(0),
+    // if null sleep the thread for 5ms
+    response_string: ?[]const u8 = null,
+    mutex: std.Io.Mutex = .init,
+};
 
-    const client = initClient(io, allocator);
-    client.deinit();
+// NOTE: determine whether it wants random data for fixed data and if it's random then take the struct see the requirements
+// and then generate data on the fly when a thread requested for it
+
+// NOTE: use smp_allocator for the response string for memory safety
+pub fn requestHandler(ci: *ClientInterface) !void {
+    ci.deinit();
 }
 
-fn initClient(io: std.Io, allocator: std.mem.Allocator) std.http.Client {
-    return std.http.Client{
-        .io = io,
-        .allocator = allocator,
-    };
-}
+// this will be called by client to generate data
+// pub fn requestBuilder(ci: *ClientInterface) std.http.Client {}
--- src/json.zig
+++ src/json.zig
@@ -12,29 +12,49 @@ pub var raw_file: struct {
     }
 } = .{};
 
-pub fn parseJson() void {
-    const ci = Req.ClintInterface.init(std.heap.smp_allocator) catch |err| {
+pub fn parseJson() !*Req.ClientInterface {
+    const ci = Req.ClientInterface.init(std.heap.smp_allocator) catch |err| {
         std.log.err("{any}\n", .{err});
         std.process.exit(1);
     };
     const allocator = ci.arena.allocator();
 
-    var parsed = std.json.parseFromSlice(
-        Req.Client,
-        allocator,
-        raw_file.content.items,
-        .{ .ignore_unknown_fields = true },
-    ) catch |err| {
-        std.log.err("parser: {any}\n", .{err});
-        std.process.exit(1);
-    };
-    defer parsed.deinit();
+    var scanner = std.json.Scanner.initCompleteInput(allocator, raw_file.content.items);
+    defer scanner.deinit();
 
-    // NOTE: use acq_rel for write and acquire for read
-    parsed.value.atomic_repeat.store(parsed.value.repeat, .monotonic);
+    var list: std.ArrayList(Req.Client) = .empty;
+    defer list.deinit(allocator);
 
-    std.log.debug("fuzz {any}\n", .{parsed.value.fuzz});
-    std.log.debug("repeat: {d}\n", .{parsed.value.repeat});
-    std.log.debug("method: {any}\n", .{parsed.value.request.method});
-    std.log.debug("atomic counter: {d}\n", .{parsed.value.atomic_repeat.load(.acquire)});
+    var count: usize = 1;
+
+    while (true) {
+        const parsed = std.json.parseFromTokenSource(Req.Client, allocator, &scanner, .{ .ignore_unknown_fields = true }) catch |err| {
+            if (err == error.EndOfStream) break;
+            std.log.err("parse Json: {any}\n", .{err});
+            break;
+        };
+
+        std.log.debug("fuzz: {any}\n", .{parsed.value.fuzz});
+        std.log.debug("repeat: {d}\n", .{parsed.value.repeat});
+        std.log.debug("method: {any}\n", .{parsed.value.request.method});
+
+        try list.append(allocator, parsed.value);
+
+        count += 1;
+    }
+    //
+    // var parsed = std.json.parseFromSlice(
+    //     Req.Client,
+    //     allocator,
+    //     raw_file.content.items,
+    //     .{ .ignore_unknown_fields = true },
+    // ) catch |err| {
+    //     std.log.err("parser: {any}\n", .{err});
+    //     std.process.exit(1);
+    // };
+    // defer parsed.deinit();
+    //
+    ci.client = try allocator.dupe(Req.Client, list.items);
+
+    return ci;
 }

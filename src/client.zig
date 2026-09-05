71be08f8ea2107e5196a964f53022e5d6b988d91
MuhArrafifPrikusuma <muhammadarrafif1308@gmail.com>
2026-09-06 06:13:38 +0800

    fixed [client.zig] client.fetch crash client.fetch crashes when a method does not support a payload. this is easily fixed by automatically omitting payload if method is either GET or HEAD

--- src/client.zig
+++ src/client.zig
@@ -18,12 +18,10 @@ pub fn clientNet(
             std.log.debug("thread: {d} finished", .{thread_id});
             break;
         };
-        std.debug.print("options method: {?any} in thread: {d}\n", .{ opt.method, thread_id });
         const result = client.fetch(opt.*) catch |err| {
             std.log.err("Client request fetch: {any}\n", .{err});
             continue;
         };
-        std.debug.print("we ever here?\n", .{});
         if (result.status.phrase()) |res| {
             _ = res;
             // std.debug.print("test result: {s}\n", .{res});

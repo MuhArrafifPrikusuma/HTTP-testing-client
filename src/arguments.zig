30d20631b17016ca3bddfd6031a9520f0dbe32af
MuhArrafifPrikusuma <muhammadarrafif1308@gmail.com>
2026-09-10 13:42:00 +0800

    only show argument left before pretty much all basic functionality done

--- src/arguments.zig
+++ src/arguments.zig
@@ -12,18 +12,24 @@ const Show = enum {
     FAIL_CLIENT,
 };
 
-pub const DoAfterFinished = union(enum) {
+pub const DoAfter = union(enum) {
     show: Show,
     nothing: void,
-    exit: noreturn,
+    exit: void,
 };
 
-pub fn handleArgs(args: std.process.Args, io: std.Io) DoAfterFinished {
+const ArgError = error{
+    UnknownArgument,
+};
+
+pub fn handleArgs(args: std.process.Args, io: std.Io) DoAfter {
     var iter = args.iterate();
 
     var buf_writer: [1024]u8 = undefined;
     var writer = std.Io.File.stdout().writer(io, &buf_writer);
     const stdout = &writer.interface;
+
+    var todo: DoAfter = .exit;
 
     json.raw_file = .{
         .allocator = std.heap.smp_allocator,
@@ -42,16 +48,22 @@ pub fn handleArgs(args: std.process.Args, io: std.Io) DoAfterFinished {
             break;
         }
 
-        processArg(arg, stdout) catch |err| std.log.err("processArg: {any}\n", .{err});
+        todo = processArg(arg, stdout);
     }
     if (args.vector.len == 1)
         stdout.print("No arguments found!\r\nuse \x1B[2m-h\x1B[22m for help\r\n", .{}) catch |err| std.log.err("{any}\n", .{err});
 
     stdout.flush() catch |err| std.log.err("{any} -> handleArgs\n", .{err});
     std.log.debug("test: {s}\n", .{json.raw_file.content.items});
+    return todo;
 }
 
-const ArgsHandlers = *const fn (*std.Io.Writer) anyerror!DoAfterFinished;
+fn handleUnknownArgs(arg: []const u8) noreturn {
+    std.log.err("Unknown argument: \x1B[2m{s}\x1B[22m\nuse \x1B[2m-h\x1B[22m for help ", .{arg});
+    std.process.exit(1);
+}
+
+const ArgsHandlers = *const fn (*std.Io.Writer) anyerror!DoAfter;
 
 const Argument = enum {
     SHOW,
@@ -59,47 +71,88 @@ const Argument = enum {
     RUN,
 };
 
-fn processArg(arg: []const u8, stdout: *std.Io.Writer) !DoAfterFinished {
-    const validArgs = [_][]const u8{
-        "-h",
-        "run",
-        "-show",
-    };
+fn convrtToEnum(arg: []const u8) ArgError!Argument {
+    if (arg.len > 255) handleUnknownArgs(arg);
 
-    const argsFunctions = [_]ArgsHandlers{
-        help,
-        setConsume,
-        setShow,
-    };
+    var tmp_arg = if (std.mem.findScalar(u8, arg, '=')) |end| arg[0..end] else arg;
+    tmp_arg = if (std.mem.eql(u8, tmp_arg, "-h")) "HELP" else tmp_arg;
 
-    var return_value: DoAfterFinished = .nothing;
+    var buf: [256]u8 = undefined;
+    const stripped_arg = std.ascii.upperString(&buf, if (tmp_arg[0] == '-') tmp_arg[1..] else tmp_arg[0..]);
 
-    for (argsFunctions, 0..) |func, i| {
-        if (std.mem.startsWith(u8, arg, "-show")) {
-            return_value = try func(stdout);
-            return return_value;
-        }
-        if (std.ascii.eqlIgnoreCase(arg, validArgs[i])) {
-            return_value = try func(stdout);
-            return return_value;
-        }
-    }
-
-    stdout.print("\x1B[38;2;255;20;10mError: Invalid argument\x1B[0m -> \x1B[3m{s}\x1B[23m\r\nuse \x1B[2m-h\x1B[22m for help\r\n", .{arg}) catch {};
-    stdout.flush() catch {};
-    std.process.exit(1);
+    return std.meta.stringToEnum(Argument, stripped_arg) orelse ArgError.UnknownArgument;
 }
-fn help(stdout: *std.Io.Writer) !DoAfterFinished {
+
+fn getExternsion(arg: []const u8) ArgError!?[]const u8 {
+    var iter = std.mem.splitScalar(u8, arg, '=');
+    var i: usize = 0;
+
+    while (iter.next()) |ext| : (i += 1) {
+        if (i != 1) continue;
+        std.debug.print("have i ever here?\n", .{});
+        return ext;
+    }
+    if (i == 1) return null;
+    if (i > 1) return ArgError.UnknownArgument;
+    unreachable;
+}
+
+fn processArg(arg: []const u8, stdout: *std.Io.Writer) DoAfter {
+    const stripped_arg = convrtToEnum(arg) catch handleUnknownArgs(arg);
+    const extension = getExternsion(arg) catch handleUnknownArgs(arg);
+
+    return blk: {
+        switch (stripped_arg) {
+            .HELP => {
+                break :blk help(stdout) catch |err| {
+                    std.log.err("Failed to help: {any}", .{err});
+                    std.process.exit(1);
+                };
+            },
+            .RUN => break :blk setConsume(),
+            .SHOW => break :blk setShow(arg, extension),
+        }
+    };
+
+    //
+    // const validArgs = [_][]const u8{
+    //     "-h",
+    //     "run",
+    //     "-show",
+    // };
+    //
+    // const argsFunctions = [_]ArgsHandlers{
+    //     help,
+    //     setConsume,
+    //     setShow,
+    // };
+    //
+    // var return_value: DoAfterFinished = .nothing;
+    //
+    // for (argsFunctions, 0..) |func, i| {
+    //     if (std.mem.startsWith(u8, arg, "-show")) {
+    //         return_value = try func(stdout);
+    //         return return_value;
+    //     }
+    //     if (std.ascii.eqlIgnoreCase(arg, validArgs[i])) {
+    //         return_value = try func(stdout);
+    //         return return_value;
+    //     }
+    // }
+    //
+    // stdout.print("\x1B[38;2;255;20;10mError: Invalid argument\x1B[0m -> \x1B[3m{s}\x1B[23m\r\nuse \x1B[2m-h\x1B[22m for help\r\n", .{arg}) catch {};
+    // stdout.flush() catch {};
+    // std.process.exit(1);
+}
+fn help(stdout: *std.Io.Writer) !DoAfter {
     try stdout.print("test help\n", .{});
     try stdout.flush();
     return .exit;
 }
 
-fn setConsume() !DoAfterFinished {
-    if (!next_should_bfile) {
-        next_should_bfile = true;
-        return .nothing;
-    }
+fn setConsume() DoAfter {
+    next_should_bfile = true;
+    return .nothing;
 }
 
 fn consume(arg: []const u8, io: std.Io, allocator: std.mem.Allocator, list: *std.ArrayList(u8)) !void {
@@ -114,6 +167,15 @@ fn consume(arg: []const u8, io: std.Io, allocator: std.mem.Allocator, list: *std
     try reader.interface.appendRemaining(allocator, list, .limited(50 * 1024 * 1024));
 }
 
-fn setShow(arg: []const u8) !DoAfterFinished {}
+fn setShow(arg: []const u8, ext: ?[]const u8) DoAfter {
+    if (ext) |e| {
+        var buf: [256]u8 = undefined;
+        const to_upper = std.ascii.upperString(&buf, e);
 
-// pub fn show(show_response: Show, io: std.Io, allocator: std.mem.Allocator) !void {}
+        const enum_val = std.meta.stringToEnum(Show, to_upper) orelse handleUnknownArgs(arg);
+        const doaf: DoAfter = .{ .show = enum_val };
+
+        return doaf;
+    } else return .{ .show = .ALL };
+    unreachable;
+}
